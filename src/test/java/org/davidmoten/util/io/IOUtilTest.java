89e8e93dbf014230826ee8d0b12188b8000b54e4
Dave Moten <davidmoten@gmail.com>
2018-04-22 17:15:23 +1000

    test byte by byte

--- src/test/java/org/davidmoten/util/io/IOUtilTest.java
+++ src/test/java/org/davidmoten/util/io/IOUtilTest.java
@@ -7,6 +7,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -38,6 +39,33 @@ public class IOUtilTest {
                         .collect(Collectors.toList());
         assertEquals("hi there", list.get(0));
         assertEquals(1, list.size());
+    }
+
+    @Test
+    public void testRoundTripGzipBytePerByte() throws IOException {
+        String s = "hi there";
+        ByteArrayInputStream a = new ByteArrayInputStream(s.getBytes(StandardCharsets.UTF_8));
+        InputStream b = IOUtil.pipe(a, o -> new GZIPOutputStream(new ByByteOutputStream(o)));
+        List<String> list = new BufferedReader(
+                new InputStreamReader(new GZIPInputStream(b), StandardCharsets.UTF_8)).lines()
+                        .collect(Collectors.toList());
+        assertEquals("hi there", list.get(0));
+        assertEquals(1, list.size());
+    }
+
+    private static final class ByByteOutputStream extends OutputStream {
+
+        private final OutputStream os;
+
+        ByByteOutputStream(OutputStream os) {
+            this.os = os;
+        }
+
+        @Override
+        public void write(int b) throws IOException {
+            os.write(b);
+        }
+
     }
 
 }
