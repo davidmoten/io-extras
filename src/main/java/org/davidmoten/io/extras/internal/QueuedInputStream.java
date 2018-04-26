8d4211b663ff4ef3fa41264b8fc53c984ae45847
Dave Moten <davidmoten@gmail.com>
2018-04-26 22:40:19 +1000

    output dev

--- src/main/java/org/davidmoten/io/extras/internal/QueuedInputStream.java
+++ src/main/java/org/davidmoten/io/extras/internal/QueuedInputStream.java
@@ -5,14 +5,22 @@ import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 public final class QueuedInputStream extends InputStream {
 
     private final byte[] singleByte = new byte[1];
     private final Deque<ByteBuffer> queue = new ArrayDeque<>();
     private boolean closed;
+    private CountDownLatch latch = new CountDownLatch(1);
+    private final AtomicInteger count = new AtomicInteger();
+    private final ReentrantLock lock = new ReentrantLock();
 
     public QueuedInputStream() {
+        lock.lock();
     }
 
     @Override
@@ -27,8 +35,27 @@ public final class QueuedInputStream extends InputStream {
 
     @Override
     public int read(byte[] b, int off, int len) throws IOException {
-        // TODO Auto-generated method stub
-        return super.read(b, off, len);
+        if (len == 0) {
+            return 0;
+        }
+        try {
+            lock.tryLock(Long.MAX_VALUE, TimeUnit.DAYS);
+            ByteBuffer bb = queue.poll();
+            int n = Math.min(bb.remaining(), len);
+            bb.get(b, off, n);
+            latch = new CountDownLatch(1);
+            if (bb.remaining() > 0) {
+                add(bb);
+            }
+            return n;
+        } catch (InterruptedException e) {
+            throw new IOException(e);
+        }
+    }
+
+    public void add(ByteBuffer bb) {
+        queue.offer(bb);
+        lock.unlock();
     }
 
     @Override
@@ -60,10 +87,6 @@ public final class QueuedInputStream extends InputStream {
     @Override
     public boolean markSupported() {
         return false;
-    }
-
-    public void add(ByteBuffer bb) {
-        queue.offer(bb);
     }
 
 }
